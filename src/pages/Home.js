d2ff3461932f41ca09e43232cabf6ee3dbfe000d
GuilhermeGM21 <guilherme_mendes_d@hotmail.com>
2022-04-13 03:40:33 -0300

    quase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { db, auth } from '../firebase';
+import { collection, query, where, onSnapshot} from 'firebase/firestore';
+import User from '../components/User';
 
 const Home = () => {
+
+  const [users, setUsers] = useState([]);
+  const [chat, setChat] = useState("");
+
+  useEffect(() => {
+    const usersRef = collection(db, 'users');
+    const q = query(usersRef, where('uid', 'not-in', [auth.currentUser.uid]));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let users = []
+      querySnapshot.forEach((doc) => {
+        users.push(doc.data())
+      });
+      setUsers(users);
+    });
+    return () => unsub();
+  }, []);
+
+  const selectUser = (user) => {
+    setChat(user);
+    console.log(user)
+  }
+
   return (
-    <div>
-        Home Page
+    <div className='home_container'>
+        <div className='users_container'>
+          {users.map(user => <User key={user.uid} user={user} selectUser={selectUser} />)}
+        </div>
+        <div className='messages_container'>
+          {chat ? <div className='messages_user'>
+            <h3>{chat.name}</h3>
+          </div> : <h3 className='no_conv'>Selecione um usuário para iniciar uma conversa</h3>}
+        </div>
     </div>
   )
 }
