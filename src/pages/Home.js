b4a730f62403ab9078bf7cfb31eee9447a5862d7
GuilhermeGM21 <guilherme_mendes_d@hotmail.com>
2022-04-14 17:36:13 -0300

    quaaase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { db, auth } from '../firebase';
-import { collection, query, where, onSnapshot} from 'firebase/firestore';
+import { db, auth, storage } from '../firebase';
+import { collection, query, where, onSnapshot, addDoc, Timestamp, orderBy } from 'firebase/firestore';
+import { ref, getDownloadURL, uploadBytes } from 'firebase/storage';
 import User from '../components/User';
+import MessageForm from '../components/MessageForm';
+import Message from '../components/Message';
 
 const Home = () => {
 
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState("");
+  const [text, setText] = useState('');
+  const [img, setImg] = useState('');
+  const [msgs, setMsgs] = useState([]);
+
+  const user1 = auth.currentUser.uid
 
   useEffect(() => {
     const usersRef = collection(db, 'users');
-    const q = query(usersRef, where('uid', 'not-in', [auth.currentUser.uid]));
+    const q = query(usersRef, where('uid', 'not-in', [user1]));
     const unsub = onSnapshot(q, (querySnapshot) => {
       let users = []
       querySnapshot.forEach((doc) => {
@@ -23,7 +31,45 @@ const Home = () => {
 
   const selectUser = (user) => {
     setChat(user);
-    console.log(user)
+
+    const user2 = user.uid;
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+    const msgsRef = collection(db, 'messages', id, 'chat');
+    const q = query(msgsRef, orderBy('createdAt', 'asc'));
+
+    onSnapshot(q, querySnapshot => {
+      let msgs = []
+      querySnapshot.forEach(doc => {
+        msgs.push(doc.data())
+      })
+      setMsgs(msgs)
+    })
+  };
+
+  console.log(msgs)
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const user2 = chat.uid
+    const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`
+
+    let url;
+    if (img) {
+      const imgRef = ref(storage, `images/${new Date().getTime()} - ${img.name}`);
+      const snap = await uploadBytes(imgRef, img);
+      const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+      url = dlUrl;
+    }
+
+    await addDoc(collection(db, 'messages', id, 'chat'), {
+      text,
+      from: user1,
+      to: user2,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || "",
+    });
+    setText('');
   }
 
   return (
@@ -32,9 +78,16 @@ const Home = () => {
           {users.map(user => <User key={user.uid} user={user} selectUser={selectUser} />)}
         </div>
         <div className='messages_container'>
-          {chat ? <div className='messages_user'>
+          {chat ? (
+            <>
+            <div className='messages_user'>
             <h3>{chat.name}</h3>
-          </div> : <h3 className='no_conv'>Selecione um usuário para iniciar uma conversa</h3>}
+            </div>
+            <div className='messages'>
+              {msgs.length ? msgs.map((msg, i) => <Message key={i} msg={msg} />) : null}
+            </div>
+            <MessageForm handleSubmit={handleSubmit} text={text} setText={setText} setImg={setImg} />
+            </>) : (<h3 className='no_conv'>Selecione um usuário para iniciar uma conversa</h3>)}
         </div>
     </div>
   )
